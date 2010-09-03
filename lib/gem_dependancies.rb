2902d3f53cb920938671daeb580dbd1d1282b371
Ryan Wilcox <rwilcox@wilcoxd.com>
2010-09-03 19:38:59 -0400

    use gem dependancies to declare what gems this package needs (should eventually be moved to the Gem config so these are auto installed)

---
+++ lib/gem_dependancies.rb
@@ -0,0 +1,6 @@
+require 'rubygems'
+
+# Required to Run the gem
+# TODO: hook this into Rakefile?
+
+gem 'json_pure', '1.4.6'
--- lib/polleverywhere_api.rb
+++ lib/polleverywhere_api.rb
@@ -1,6 +1,8 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+require 'gem_dependancies'
+
 require 'polleverywhere_api/user'
 module PolleverywhereApi
   VERSION = '0.0.1'
