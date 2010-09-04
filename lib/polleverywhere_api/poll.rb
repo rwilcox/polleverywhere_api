9fe7a7208a52eb1061eddfffb32209f6a9b3f697
Ryan Wilcox <rwilcox@wilcoxd.com>
2010-09-03 21:21:08 -0400

    poll keywords implemented

--- lib/polleverywhere_api/poll.rb
+++ lib/polleverywhere_api/poll.rb
@@ -4,8 +4,12 @@ class Poll
   attr_accessor :state
   attr_accessor :title
   attr_accessor :type
+  attr_reader :user
 
-  def initialize(permalink, result_count, state, title, type)
+  include ConnectionMixin
+
+  def initialize(user, permalink, result_count, state, title, type)
+    @user = user
     self.permalink = permalink
     self.result_count = result_count
     self.state = state
@@ -13,8 +17,26 @@ class Poll
     self.type = type
   end
 
+  # right now we assume this is a free text poll, because that's all I need
+  # but a multiple choice poll will have an options attribute in the json
+  def keywords
+    parse_response( send_request("free_text_polls/#{self.permalink}", :json) )["keyword"]
+  end
+
   def vote(vote)
     
   end
 
+  def username
+    user.username
+  end
+
+  def password
+    user.password
+  end
+
+private
+  def send_detail_request
+    
+  end
 end
