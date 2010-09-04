3f5dbdd25bb86cb2a55c0a13afd7c979292ab41f
Ryan Wilcox <rwilcox@wilcoxd.com>
2010-09-03 20:21:59 -0400

    include stuff in polleverywhere_api folder for ease of reference

--- lib/polleverywhere_api.rb
+++ lib/polleverywhere_api.rb
@@ -1,9 +1,15 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+path_to_code = File.dirname(__FILE__) + "/polleverywhere_api/"
+$:.unshift(path_to_code) unless
+  $:.include?(path_to_code) || $:.include?(File.expand_path(path_to_code))
+
 require 'gem_dependancies'
 
 require 'polleverywhere_api/user'
+
+
 module PolleverywhereApi
   VERSION = '0.0.1'
 end
