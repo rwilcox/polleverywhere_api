9cb0cef136e6d9211e5ccf338fcc42d85bdef23c
Ryan Wilcox <rwilcox@wilcoxd.com>
2010-09-06 20:28:42 -0400

    can pull keywords and results out of Multiple Choice Polls

--- lib/polleverywhere_api/poll_multiple_choice.rb
+++ lib/polleverywhere_api/poll_multiple_choice.rb
@@ -4,8 +4,24 @@ module PollEverywhere
   
 class MultipleChoicePoll < Poll
 
+  def poll_detail_url
+    "multiple_choice_polls/#{self.permalink}"
+  end
+
+  # will return an Array of the keywords (aka choices) you have for this poll
+  def keywords
+    sef.results.collect {|current_object| current_object["keyword"] }
+  end
+
+
+  # returns a list of hashes. The hashes have the following keys:
+  # id, value, results_percentage, results_count, keyword, 
+  def results
+    parse_response( send_request(self.poll_detail_url, :json) )["options"]
+  end
+
+
   def vote(keyword)
-    puts "sending: " + "interpret.json?response=#{keyword}"
     parse_response( send_request("interpret.json?response=#{keyword}", nil) )
   end
 end
